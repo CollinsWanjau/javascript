d07b1da3e33183827c28ab52d8fe6c3faf513932
colloso999 <tripperskripper@gmail.com>
2022-12-10 13:34:36 +0300

    Created function apply

---
+++ jsTutorials/apply.js
@@ -0,0 +1,14 @@
+// the apply() accepts args as an array
+//
+const person = {
+  fullName: function(city, country) {
+    return this.firstName + " " + this.lastName + "," + city + "," + country;
+  }
+}
+
+const person1 = {
+  firstName:"John",
+  lastName: "Doe"
+}
+
+console.log(person.fullName.apply(person1, ["Oslo", "Norway"]))
