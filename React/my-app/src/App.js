00fba90012e98ad372da56e019330a3f16670c09
colloso999 <tripperskripper@gmail.com>
2024-02-06 13:15:18 +0300

    Add App component and import Profile component

---
+++ React/my-app/src/App.js
@@ -0,0 +1,17 @@
+
+import './App.css';
+import Profile from './MyDog'
+
+
+function App() {
+  return (
+    <div >
+      <h1>Welcome to my app</h1>
+      {/* Use button component */}
+      <Profile />
+      
+    </div>
+  );
+}
+
+export default App;
