a23483a9ec5f49b6c871b8510561d4703acb46dc
colloso999 <tripperskripper@gmail.com>
2024-02-07 09:28:27 +0300

    Add ShoppingList and MyButton components, update state and event handling in App.js

--- React/my-app/src/App.js
+++ React/my-app/src/App.js
@@ -1,15 +1,25 @@
-
-import './App.css';
-import Profile from './MyDog'
-
+import ShoppingList from "./ShoppingList";
+// import Profile from './MyDog'
+import MyButton from "./MyButton";
+import React, { useState } from 'react'
 
 function App() {
+
+  // Updating the Screen
+    const [count, setCount] = useState(0)
+
+    // Responding to events
+    function handleClick() {
+        setCount(count + 1);
+    }
   return (
     <div >
       <h1>Welcome to my app</h1>
       {/* Use button component */}
-      <Profile />
-      
+      <MyButton count={count} onClick={handleClick} />
+      <MyButton count={count} onClick={handleClick} />
+      {/* <Profile /> */}
+      <ShoppingList />
     </div>
   );
 }
