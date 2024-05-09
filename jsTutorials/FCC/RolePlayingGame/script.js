7bc7d75a552b675675f64a80407ab4af83f6d3ce
colloso999 <tripperskripper@gmail.com>
2024-05-09 12:24:44 +0300

    Add monster defeat functionality and update fight logic

--- jsTutorials/FCC/RolePlayingGame/script.js
+++ jsTutorials/FCC/RolePlayingGame/script.js
@@ -71,6 +71,18 @@ const locations = [
         "button text": ["Attack", "Dodge", "Run"],
         "button functions": [attack, dodge, goTown],
         text: "You are fighting a monster."
+    },
+    {
+        name: "kill monster",
+        "button text": ["Go to town square", "Go to town square", "Go to town square"],
+        "button functions": [goTown, goTown, goTown],
+        text: 'The monster screams "Arg!" as it dies. You gain experience points and find gold.'
+    },
+    {
+        name: "lose",
+        "button text": ["REPLAY?", "REPLAY?", "REPLAY?"],
+        "button functions": [restart, restart, restart],
+        text: "You die. &#x2620;"
     }
 ]
 
@@ -82,6 +94,7 @@ button2.onclick = goCave;
 button3.onclick = fightDragon;
 
 function update(location) {
+    monsterStats.style.display = "none"
     const button1 = document.querySelector('#button1')
     button1.innerText = location["button text"][0]
     button2.innerText = location["button text"][1]
@@ -183,14 +196,56 @@ function fightDragon() {
     goFight()
 }
 
+/**
+ * Function to initiate a fight.
+ * @function goFight
+ */
 function goFight() {
-
+    update(locations[3])
+    monsterHealth = monsters[fighting].health
+    monsterStats.style.display = 'block'
+    monsterName.innerText = monsters[fighting].name
+    monsterHealthText.innerText = monsterHealth
 }
 
 function attack() {
-
+    text.innerText = 'The ' + monsters[fighting].name + ' attacks.'
+    text.innerText += " You attack it with your " + weapons[currentWeapon].name +  "."
+    health -= monsters[fighting].level
+    monsterHealth -= (weapons[currentWeapon].power + Math.floor(Math.round() * xp) + 1)
+    healthText.innerText = health
+    monsterHealthText.innerText = monsterHealth
+    if (health <= 0) {
+        lose()
+    } else if(monsterHealth <= 0) {
+        defeatMonster()
+    }
 }
 
 function dodge() {
+    text.innerText = 'You dodge the attack from the ' + monsters[fighting].name
+}
 
+function defeatMonster() {
+    gold += Math.floor(monsters[fighting].level * 6.7)
+    xp += monsters[fighting].level
+    goldText.innerText = gold
+    xpText.innerText = xp
+    update(locations[4])
+}
+
+function lose() {
+    update(locations[5])
+}
+
+function restart() {
+    xp = 0
+    health = 100
+    gold = 50
+    currentWeapon = 0
+    inventory = ['stick']
+    goldText.innerText = gold
+    healthText.innerText = health
+    xpText.innerText = xp
+    goTown()
 }
