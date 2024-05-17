cb42502d6c916eedadd407ae007fc1893d44f288
colloso999 <tripperskripper@gmail.com>
2024-05-17 23:11:48 +0300

    Remove unused JavaScript tutorial files

--- jsTutorials/forOf.js
+++
@@ -1,8 +0,0 @@
-const cars = ["BMW", "Volvo", "Mini"];
-
-// list all elements
-let text = "";
-for (let x of cars) {
-	text += x + '\n';
-}
-console.log(text);
--- jsTutorials/hoisting.js
+++
@@ -1,5 +0,0 @@
-// self invoking function
-(function() {
-	let x = "Hello";	// i will invoke myself
-	console.log(x);
-})();
