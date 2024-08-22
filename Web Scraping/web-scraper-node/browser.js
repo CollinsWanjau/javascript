115e745cd16dd526f6a794063d476c886181c03e
colloso999 <tripperskripper@gmail.com>
2024-08-22 23:34:01 +0300

    A script that starts the browser

---
+++ Web Scraping/web-scraper-node/browser.js
@@ -0,0 +1,24 @@
+const puppeteer = require('puppeteer-core');
+
+/* Start with browser.js; this file will contain the script that starts your browser. */
+async function startBrowser() {
+	let browser;
+	try {
+		console.log('Opening the browser......');
+
+		/* launch() method that launches an instance of a browser.This method returns a Promise */
+		browser = await puppeteer.launch({
+			executablePath: '/usr/bin/chromium-browser',
+			headless: false,
+			args: ["--disable-setuid-sandbox"],
+			'ignoreHTTPSErrors': true
+		});
+	} catch (err) {
+		console.log("Could not create a browser instance => : ", err);
+	}
+	return browser;
+}
+
+module.exports = {
+	startBrowser
+};
