d171b26063eb2d6f76ff3fc952475e3cdb0dd928
colloso999 <tripperskripper@gmail.com>
2024-09-09 07:59:59 +0300

    Selecting the category of book to be displayed

--- Web Scraping/web-scraper-node/pageScraper.js
+++ Web Scraping/web-scraper-node/pageScraper.js
@@ -3,10 +3,20 @@
 // created a page instance and then used the page.goto() method to navigate to the url
 const scraperObject = {
   url: 'http://books.toscrape.com',
-  async scraper (browser) {
+  async scraper (browser, category) {
     const page = await browser.newPage();
     console.log(`Navigating to ${this.url}...`);
+    // Navigate to the selected page
     await page.goto(this.url);
+    // Select the category of book to be displayed
+    let selectedCategory = await page.$$eval('.side_categories > ul > li > ul > li > a', (links, _category) => {
+	    // Search the item that has the matching text
+	    links.map(a => a.textContent.replace(/(\r\n\t|\n|\r|\t|^\s|\s$|\B\s|\s\B)/gm, "") === _category ? a : null);
+	    let link = links.filter(tx => tx !== null)[0]
+	    return link.href
+    }, category)
+    // Navigate to the selected category
+    await page.goto(selectedCategory)
     const scrapedData = [];
     // Wait for the required DOM to be rendered
     async function scrapeCurrentPage () {
@@ -44,32 +54,32 @@ const scraperObject = {
 
       for (link in urls) {
         const currentPageData = await pagePromise(urls[link]);
-        scrapedData.push(currentPageData)
+        scrapedData.push(currentPageData);
         /* console.log(currentPageData); */
       }
 
       // When all the data on this page is done, click the next button and start the sraping of the next
       // page
       // You are going to check if this button exist first, so you know if there really is a next page
-      let nextButtonExist = false
+      let nextButtonExist = false;
       try {
-        const nextButton = await page.$eval('.next > a', a => a.textContent)
-        nextButtonExist = true
-      } catch(err) {
-        nextButtonExist = false
+        const nextButton = await page.$eval('.next > a', a => a.textContent);
+        nextButtonExist = true;
+      } catch (err) {
+        nextButtonExist = false;
       }
-      if(nextButtonExist) {
-        await page.click('.next > a')
-        return scrapeCurrentPage()  // Call this function recursively
+      if (nextButtonExist) {
+        await page.click('.next > a');
+        return scrapeCurrentPage(); // Call this function recursively
       }
 
-      await page.close()
-      return scrapedData
+      await page.close();
+      return scrapedData;
     }
 
-    let data = await scrapeCurrentPage()
-    console.log(data)
-    return data
+    const data = await scrapeCurrentPage();
+    console.log(data);
+    return data;
     console.log(urls);
   }
 };
